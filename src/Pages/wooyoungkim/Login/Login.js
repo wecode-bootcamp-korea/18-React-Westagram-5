c7fdc32b9a18cc387999db71c17c5d9cbe55e7a6
wooyoung <whoyoung90@naver.com>
2021-03-05 19:58:37 +0900

    210305 초기 세팅

--- src/Pages/wooyoungkim/Login/Login.js
+++ src/Pages/wooyoungkim/Login/Login.js
@@ -3,7 +3,7 @@ import './Login.scss';
 import { Link } from 'react-router-dom';
 
 
-class Loginwoo extends Component {
+class Loginwoo extends Component {   
 	state = {  }
 	render() { 
 		return (
