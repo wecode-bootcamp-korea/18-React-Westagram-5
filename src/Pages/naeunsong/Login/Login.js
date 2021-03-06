7820e4f7bb5c87bf06d7aa253e86fb8b43df939a
songbetter <5ongnaeu17@gmail.com>
2021-03-06 13:51:58 +0900

    Add: Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/Pages/naeunsong/Login/Login.js
+++ src/Pages/naeunsong/Login/Login.js
@@ -8,13 +8,27 @@ class Loginsong extends React.Component {
     this.props.history.push('/main-naeunsong');
   } 
 
+  constructor(){
+    super();
+    this.state ={
+      userId: '',
+      userPassword: ''
+    }
+  }
+
+  handleIdInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  
   render(){
     return (
     <section className="login">
       <h1 className="logo">westagram</h1>
       <form>
-        <input name="userId" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-        <input name="userPassword" type="password" placeholder="비밀번호" />
+        <input onChange={this.handleIdInput} value={this.state.userId} name="userId" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
+        <input onChange={this.handleIdInput} value={this.state.userPassword} name="userPassword" type="password" placeholder="비밀번호" />
         <button onClick={this.goToMain}>로그인</button>
       </form>
       <a href="https://www.instagram.com/accounts/password/reset/" className="checkPassword">비밀번호를 잊으셨나요?</a>
