ad6e79792e31e6297035377c2477355f1c4bc86c
songbetter <5ongnaeu17@gmail.com>
2021-03-06 15:10:17 +0900

    Modify: Header컴포넌트 경로 수정

--- src/Pages/naeunsong/Main/Main.js
+++ src/Pages/naeunsong/Main/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Main.scss";
 import "../../../Styles/common.scss";
-import Header from "../../../Components/Header";
+import Header from "./Header/Header";
 import Feeds from "./Feeds/Feeds";
 import MainRight from "./MainRight/MainRight";
 
