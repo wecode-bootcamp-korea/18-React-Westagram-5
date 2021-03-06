b77ec16c490f7fa50ebf8e2bbafa5d6b0e09ecd5
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2021-03-06 15:36:16 +0900

    기능 구현 전 으로 다시 수정

--- src/Pages/geunhwalee/Login/Login.js
+++ src/Pages/geunhwalee/Login/Login.js
@@ -6,29 +6,13 @@ import './Login.scss';
 
 
 class Loginlee extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			id: "",
-			pw: ""
-		};
-	}
+
 	
     goToMain = () => {
         this.props.history.push('/main-geunhwalee');
     }
 
-	handleIdInput = (e) => {
-		this.setState ({
-			id: e.target.value,
-		});
-	}
-
-	handlePwInput = (e) => {
-		this.setState({
-			pw: e.target.value,
-		});
-	}	
+	
 
 
     render() {
@@ -37,13 +21,13 @@ class Loginlee extends React.Component {
                 <section className="background">
                     <form className="loginBox">
                         <h1>westagram</h1>
-                        <input type="email" className ="username" placeholder="전화번호, 사용자 이름 또는 이메일" onChange={this.handleIdInput} />
-                        <input type="password" className="pswd" placeholder="비밀번호" onChange={this.handlePwInput} />
+                        <input type="email" className ="username" placeholder="전화번호, 사용자 이름 또는 이메일"  />
+                        <input type="password" className="pswd" placeholder="비밀번호"  />
                         <button className="loginBtn" onClick={this.goToMain}>로그인</button> 
                         <a href="https://www.instagram.com/accounts/password/reset/" id="pwdAsk">비밀번호를 잊으셨나요?</a>
                     </form>
                 </section>
-            </div>
+            </div> 
         )
     }
 }
