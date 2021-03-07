6f27cc0317d4b25d69aec49a463ff051af39297c
wooyoung <whoyoung90@naver.com>
2021-03-07 16:19:35 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/Pages/wooyoungkim/Login/Login.js
+++ src/Pages/wooyoungkim/Login/Login.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import './Login.scss';
 import { Link } from 'react-router-dom';
+import './Login.scss';
 
 
-class Loginwoo extends Component {   
-	state = {  }
+class Loginwoo extends Component {
+  constructor(){
+    super();
+    this.state = {
+      id: "",
+      pw: "" 
+    };
+  }
+
+  handleIdInput = (event) => {
+    this.setState({ id: event.target.value });
+  }
+
+  handlePwInput = (event) => {
+    this.setState({ pw : event.target.value });
+  }
+
 	render() { 
 		return (
-			<div class="Login">
-            <div class='westagram'>westagram</div>
-            <div class="login_border">
-              <input class="login_txt" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-              <input class="login_pwd" type="password" placeholder="비밀번호" />
-              <button class="login_btn" onclick="moveMain()" disabled><Link to="/Main">로그인</Link></button>
+			<div className="Login">
+            <div className='westagram'>westagram</div>
+            <div className="login_border">
+              <input className="login_txt" type="text" onChange={this.handleIdInput} placeholder="전화번호, 사용자 이름 또는 이메일" />
+              <input className="login_pwd" type="password" onChange={this.handlePwInput} placeholder="비밀번호" />
+              <button className="login_btn" onClick="moveMain()" disabled><Link to="/main-wooyoungkim">로그인</Link></button>
             </div>
             <a href="#">비밀번호를 잊으셨나요?</a>
           </div>
