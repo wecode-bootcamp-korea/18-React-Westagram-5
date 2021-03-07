68d9fa3576dc3e99e6411ee9ddd37a61df689927
wooyoung <whoyoung90@naver.com>
2021-03-08 00:16:41 +0900

    Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/Pages/wooyoungkim/Login/Login.js
+++ src/Pages/wooyoungkim/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './Login.scss';
 
 
@@ -8,7 +8,7 @@ class Loginwoo extends Component {
     super();
     this.state = {
       id: "",
-      pw: "" 
+      pw: "",
     };
   }
 
@@ -17,22 +17,34 @@ class Loginwoo extends Component {
   }
 
   handlePwInput = (event) => {
-    this.setState({ pw : event.target.value });
+    this.setState({ pw: event.target.value });
   }
 
-	render() { 
+  goToMain = () => {
+    const {id, pw} = this.state;
+    
+    if( id.includes("@") && pw.length>=5 ){
+      this.props.history.push("/main-wooyoungkim");
+    }
+  }
+  
+	render() {
+    const { id, pw } = this.state;
+
 		return (
 			<div className="Login">
-            <div className='westagram'>westagram</div>
-            <div className="login_border">
-              <input className="login_txt" type="text" onChange={this.handleIdInput} placeholder="전화번호, 사용자 이름 또는 이메일" />
-              <input className="login_pwd" type="password" onChange={this.handlePwInput} placeholder="비밀번호" />
-              <button className="login_btn" onClick="moveMain()" disabled><Link to="/main-wooyoungkim">로그인</Link></button>
-            </div>
-            <a href="#">비밀번호를 잊으셨나요?</a>
-          </div>
+        <div className='westagram'>westagram</div>
+        <div className="login_border">
+          <input className="login_txt" type="text" onChange={this.handleIdInput} placeholder="전화번호, 사용자 이름 또는 이메일" />
+          <input className="login_pwd" type="password" onChange={this.handlePwInput} placeholder="비밀번호" />
+          <button className={ id.includes("@") && pw.length>=5  ? "loginBtnAble" : "loginBtnUnable"} 
+                  type="submit" 
+                  onClick={this.goToMain}>로그인</button>
+        </div>
+          <a href="#">비밀번호를 잊으셨나요?</a>
+      </div>
 		);
 	}
 }
  
-export default Loginwoo;
+export default withRouter(Loginwoo);
