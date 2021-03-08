4b79d887999c862ed8808e183e30fbf05d464751
songbetter <5ongnaeu17@gmail.com>
2021-03-08 20:37:13 +0900

    Modify: Mission 2 - 로그인 버튼 활성화 기능 수정

--- src/Pages/naeunsong/Login/Login.js
+++ src/Pages/naeunsong/Login/Login.js
@@ -22,18 +22,15 @@ class Loginsong extends React.Component {
     });
   }
   
-  activeBtn = () => {
-    return (this.state.userId.includes('@') && (this.state.userPassword.length >= 5)) ? 'activeBtn' : ''
-    }
-
   render(){
+    const { userId, userPassword } = this.state
     return (
     <section className="login">
       <h1 className="logo">westagram</h1>
       <form>
         <input onChange={this.handleIdInput} value={this.state.userId} name="userId" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
         <input onChange={this.handleIdInput} value={this.state.userPassword} name="userPassword" type="password" placeholder="비밀번호" />
-        <button onClick={this.goToMain} className={this.activeBtn()}>로그인</button>
+        <button onClick={this.goToMain} className={userId.includes('@') && (userPassword.length >= 5) ? 'activeBtn' : 'btnOff'}>로그인</button>
       </form>
       <a href="https://www.instagram.com/accounts/password/reset/" className="checkPassword">비밀번호를 잊으셨나요?</a>
     </section>
